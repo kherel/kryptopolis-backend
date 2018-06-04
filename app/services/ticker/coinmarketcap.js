d6df088f212b7c19bbe5b5c596c0d7dfc4981cb5
stan <s.k.golovin@gmail.com>
2018-06-04 13:47:43 +0700

    + 100 tickers per request

--- app/services/ticker/coinmarketcap.js
+++ app/services/ticker/coinmarketcap.js
@@ -2,46 +2,53 @@ import request from 'superagent'
 import { Ticker } from '../../../init/mongoose'
 
 export const getTickers = async (path) => {
-  let tickers = await request
-    .get('https://api.coinmarketcap.com/v2/ticker/')
+  let num = 100;
 
-  tickers = JSON.parse(tickers.text)
+  for (var start = 0, tries = 0; start < num || tries < 30; start += 100, ++i) {
 
-  if (!tickers["metadata"]["error"]) {
+    let tickers = await request
+      .get('https://api.coinmarketcap.com/v2/ticker/&start=' + start + '&limit=100&sort=id')
 
-    for (var k in tickers["data"]) {
-      const name = tickers["data"][k]["name"]
-      const ticker = await Ticker.findOne({ name })
+    tickers = JSON.parse(tickers.text)
 
-      try {
-        if (!ticker) {
-          let tmp = {}
+    num = tickers["metadata"]["num_cryptocurrencies"]
 
-          tmp.name = tickers["data"][k]["name"]
-          tmp.symbol = tickers["data"][k]["symbol"]
-          tmp.priceUSD = tickers["data"][k]["quotes"]["USD"]["price"]
-          tmp.volume24h = tickers["data"][k]["quotes"]["USD"]["volume_24h"]
-          tmp.marketCap = tickers["data"][k]["quotes"]["USD"]["market_cap"]
-          tmp.percentChange1h = tickers["data"][k]["quotes"]["USD"]["percent_change_1h"]
-          tmp.percentChange24h = tickers["data"][k]["quotes"]["USD"]["percent_change_24h"]
-          tmp.percentChange7d = tickers["data"][k]["quotes"]["USD"]["percent_change_7d"]
-          tmp.lastUpdated = tickers["data"][k]["last_updated"]
+    if (!tickers["metadata"]["error"]) {
 
-          await Ticker.createObject(tmp)
-        } else {
-          ticker.priceUSD = tickers["data"][k]["quotes"]["USD"]["price"]
-          ticker.volume24h = tickers["data"][k]["quotes"]["USD"]["volume_24h"]
-          ticker.marketCap = tickers["data"][k]["quotes"]["USD"]["market_cap"]
-          ticker.percentChange1h = tickers["data"][k]["quotes"]["USD"]["percent_change_1h"]
-          ticker.percentChange24h = tickers["data"][k]["quotes"]["USD"]["percent_change_24h"]
-          ticker.percentChange7d = tickers["data"][k]["quotes"]["USD"]["percent_change_7d"]
-          ticker.lastUpdated = tickers["data"][k]["last_updated"]
+      for (var k in tickers["data"]) {
+        const name = tickers["data"][k]["name"]
+        const ticker = await Ticker.findOne({ name })
 
-          await ticker.save()
+        try {
+          if (!ticker) {
+            let tmp = {}
+
+            tmp.name = tickers["data"][k]["name"]
+            tmp.symbol = tickers["data"][k]["symbol"]
+            tmp.priceUSD = tickers["data"][k]["quotes"]["USD"]["price"]
+            tmp.volume24h = tickers["data"][k]["quotes"]["USD"]["volume_24h"]
+            tmp.marketCap = tickers["data"][k]["quotes"]["USD"]["market_cap"]
+            tmp.percentChange1h = tickers["data"][k]["quotes"]["USD"]["percent_change_1h"]
+            tmp.percentChange24h = tickers["data"][k]["quotes"]["USD"]["percent_change_24h"]
+            tmp.percentChange7d = tickers["data"][k]["quotes"]["USD"]["percent_change_7d"]
+            tmp.lastUpdated = tickers["data"][k]["last_updated"]
+
+            await Ticker.createObject(tmp)
+          } else {
+            ticker.priceUSD = tickers["data"][k]["quotes"]["USD"]["price"]
+            ticker.volume24h = tickers["data"][k]["quotes"]["USD"]["volume_24h"]
+            ticker.marketCap = tickers["data"][k]["quotes"]["USD"]["market_cap"]
+            ticker.percentChange1h = tickers["data"][k]["quotes"]["USD"]["percent_change_1h"]
+            ticker.percentChange24h = tickers["data"][k]["quotes"]["USD"]["percent_change_24h"]
+            ticker.percentChange7d = tickers["data"][k]["quotes"]["USD"]["percent_change_7d"]
+            ticker.lastUpdated = tickers["data"][k]["last_updated"]
+
+            await ticker.save()
+          }
+        } catch (err) {
+          console.log(err)
+          continue
         }
-      } catch (err) {
-        console.log(err)
-        continue
       }
     }
   }
