0578f535c41a9cd92a83720a291a8b3d67712933
stan <s.k.golovin@gmail.com>
2018-06-04 14:15:32 +0700

    + try\catch tickers job

--- app/services/ticker/coinmarketcap.js
+++ app/services/ticker/coinmarketcap.js
@@ -4,52 +4,57 @@ import { Ticker } from '../../../init/mongoose'
 export const getTickers = async (path) => {
   let num = 100;
 
-  for (let start = 0, tries = 0; start < num || tries < 30; start += 100, ++tries) {
+  try {
 
-    let tickers = await request
-      .get('https://api.coinmarketcap.com/v2/ticker/?start=' + start + '&limit=100&sort=id')
+    for (let start = 0, tries = 0; start < num || tries < 30; start += 100, ++tries) {
 
-    tickers = JSON.parse(tickers.text)
+      let tickers = await request
+        .get('https://api.coinmarketcap.com/v2/ticker/?start=' + start + '&limit=100&sort=id')
 
-    num = tickers["metadata"]["num_cryptocurrencies"]
+      tickers = JSON.parse(tickers.text)
 
-    if (!tickers["metadata"]["error"]) {
+      num = tickers["metadata"]["num_cryptocurrencies"]
 
-      for (let k in tickers["data"]) {
-        const name = tickers["data"][k]["name"]
-        const ticker = await Ticker.findOne({ name })
+      if (!tickers["metadata"]["error"]) {
 
-        try {
-          if (!ticker) {
-            let tmp = {}
+        for (let k in tickers["data"]) {
+          const name = tickers["data"][k]["name"]
+          const ticker = await Ticker.findOne({ name })
 
-            tmp.name = tickers["data"][k]["name"]
-            tmp.symbol = tickers["data"][k]["symbol"]
-            tmp.priceUSD = tickers["data"][k]["quotes"]["USD"]["price"]
-            tmp.volume24h = tickers["data"][k]["quotes"]["USD"]["volume_24h"]
-            tmp.marketCap = tickers["data"][k]["quotes"]["USD"]["market_cap"]
-            tmp.percentChange1h = tickers["data"][k]["quotes"]["USD"]["percent_change_1h"]
-            tmp.percentChange24h = tickers["data"][k]["quotes"]["USD"]["percent_change_24h"]
-            tmp.percentChange7d = tickers["data"][k]["quotes"]["USD"]["percent_change_7d"]
-            tmp.lastUpdated = tickers["data"][k]["last_updated"]
+          try {
+            if (!ticker) {
+              let tmp = {}
 
-            await Ticker.createObject(tmp)
-          } else {
-            ticker.priceUSD = tickers["data"][k]["quotes"]["USD"]["price"]
-            ticker.volume24h = tickers["data"][k]["quotes"]["USD"]["volume_24h"]
-            ticker.marketCap = tickers["data"][k]["quotes"]["USD"]["market_cap"]
-            ticker.percentChange1h = tickers["data"][k]["quotes"]["USD"]["percent_change_1h"]
-            ticker.percentChange24h = tickers["data"][k]["quotes"]["USD"]["percent_change_24h"]
-            ticker.percentChange7d = tickers["data"][k]["quotes"]["USD"]["percent_change_7d"]
-            ticker.lastUpdated = tickers["data"][k]["last_updated"]
+              tmp.name = tickers["data"][k]["name"]
+              tmp.symbol = tickers["data"][k]["symbol"]
+              tmp.priceUSD = tickers["data"][k]["quotes"]["USD"]["price"]
+              tmp.volume24h = tickers["data"][k]["quotes"]["USD"]["volume_24h"]
+              tmp.marketCap = tickers["data"][k]["quotes"]["USD"]["market_cap"]
+              tmp.percentChange1h = tickers["data"][k]["quotes"]["USD"]["percent_change_1h"]
+              tmp.percentChange24h = tickers["data"][k]["quotes"]["USD"]["percent_change_24h"]
+              tmp.percentChange7d = tickers["data"][k]["quotes"]["USD"]["percent_change_7d"]
+              tmp.lastUpdated = tickers["data"][k]["last_updated"]
 
-            await ticker.save()
+              await Ticker.createObject(tmp)
+            } else {
+              ticker.priceUSD = tickers["data"][k]["quotes"]["USD"]["price"]
+              ticker.volume24h = tickers["data"][k]["quotes"]["USD"]["volume_24h"]
+              ticker.marketCap = tickers["data"][k]["quotes"]["USD"]["market_cap"]
+              ticker.percentChange1h = tickers["data"][k]["quotes"]["USD"]["percent_change_1h"]
+              ticker.percentChange24h = tickers["data"][k]["quotes"]["USD"]["percent_change_24h"]
+              ticker.percentChange7d = tickers["data"][k]["quotes"]["USD"]["percent_change_7d"]
+              ticker.lastUpdated = tickers["data"][k]["last_updated"]
+
+              await ticker.save()
+            }
+          } catch (err) {
+            console.log(err)
+            continue
           }
-        } catch (err) {
-          console.log(err)
-          continue
         }
       }
     }
+  } catch (err) {
+    console.log(err)
   }
 }
