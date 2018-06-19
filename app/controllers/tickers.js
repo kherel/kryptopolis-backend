00c547e3183c13223086e325a25cd6eed4148a68
Kherel Kechil <kherel@gmail.com>
2018-06-19 14:00:04 +0900

    [add] new route + countoller

--- app/controllers/tickers.js
+++ app/controllers/tickers.js
@@ -1,7 +1,8 @@
-import { merge, pick } from "ramda"
-import { Ticker } from "../../init/mongoose"
-import { tickerSerializer } from "../serializers"
-import { getAttributes, getOptionsFind } from "../services/params"
+import { merge, pick } from "ramda";
+import { Ticker } from "../../init/mongoose";
+import { tickerSerializer } from "../serializers";
+import { getAttributes, getOptionsFind } from "../services/params";
+import mongoose from 'mongoose'
 
 const filterAttributes = pick([
   "name",
@@ -12,22 +13,58 @@ const filterAttributes = pick([
   "percentChange1h",
   "percentChange24h",
   "percentChange7d",
-  "lastUpdated",
-])
+  "lastUpdated"
+]);
 
 export default {
-
   index: async (req, res, next) => {
     try {
-      let tickers = await Ticker.find({}, null, getOptionsFind(req))
-      let total = await Ticker.count()
+      let tickers = await Ticker.find({}, null, getOptionsFind(req));
+      let total = await Ticker.count();
 
-      let response = tickerSerializer(tickers, { total: total })
+      let response = tickerSerializer(tickers, { total: total });
 
-      res.status(200).json(response)
-    } catch(err) {
-      return next(err)
+      res.status(200).json(response);
+    } catch (err) {
+      return next(err);
+    }
+  },
+
+  special: async (req, res, next) => {
+    try {
+      let down = await Ticker.$where("this.percentChange24h !== null")
+        .sort("percentChange24h")
+        .limit(10);
+
+      let up = await Ticker.$where("this.percentChange24h !== null")
+        .sort("-percentChange24h")
+        .limit(10);
+
+      let widget = await Ticker.find({
+        'name': ['Bitcoin', 'Ethereum', 'Ripple']
+      })
+
+      res.status(200).json(nomalizeSerializer({up, down, widget}));
+    } catch (err) {
+      return next(err);
     }
   }
+};
 
+
+function nomalizeSerializer(obj) {
+  const res = {
+    entities: {}
+  }
+
+  Object.keys(obj).map(key => {
+    res[key] = []
+    obj[key].map(item => {
+      const id = item._id
+      res.entities[id] = item
+      res[key].push(id)
+    })
+
+  })
+  return res
 }
